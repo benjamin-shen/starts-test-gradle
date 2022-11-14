7a8f6cb12c0e788231ba059740b55dc75e2ddea2
Benjamin Shen <bfs45@cornell.edu>
2022-11-13 22:15:38 -0500

    add second test

---
+++ src/test/java/first/SecondTest.java
@@ -0,0 +1,12 @@
+package first;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SecondTest {
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
